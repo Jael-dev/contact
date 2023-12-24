a30c83de56c1adc9f37f8f75067260588db69603
jael-dev <jaelngaleu@gmail.com>
2023-12-25 00:33:45 +0100

    Contact list view skeleton created

---
+++ src/components/ContactList/ContactListView.vue
@@ -0,0 +1,28 @@
+
+<script setup>
+import { ref } from 'vue';
+
+const drawer = ref(null);
+</script>
+<template>
+    <v-card>
+      <v-layout>
+        <v-navigation-drawer
+          v-model="drawer"
+          temporary
+        >
+          
+        </v-navigation-drawer>
+        <v-main style="width: 350px">
+          <div class="d-flex justify-center align-center h-100">
+            <v-btn
+              color="primary"
+              @click.stop="drawer = !drawer"
+            >
+              Toggle
+            </v-btn>
+          </div>
+        </v-main>
+      </v-layout>
+    </v-card>
+  </template>
