7c25ebafdf29c0869179682c49d01d56af441af2
jael-dev <jaelngaleu@gmail.com>
2023-12-24 22:57:34 +0100

    open github repo function, open linkedin function

---
+++ src/components/SideBar/ImportButton.vue
@@ -0,0 +1,8 @@
+
+<template v-slot:append>
+    <div class="pa-2">
+      <v-btn block>
+        Logout
+      </v-btn>
+    </div>
+  </template>
--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,11 +1,27 @@
 <script setup>
 import { ref } from 'vue';
 import AddContact from './AddContact.vue';
+import ImportButton from './ImportButton.vue';
 
 const isClicked = ref(false);
 
 const handleClick = () => {
     isClicked.value = !isClicked.value;
+};
+
+const openGithubRepo = () => {
+  const githubRepoUrl = "https://github.com/yourusername/yourrepo";
+  
+  // Open the GitHub repo link in a new tab
+  window.open(githubRepoUrl, "_blank");
+};
+
+const openLinkedinProfile = () => {
+  // Replace "https://github.com/yourusername/yourrepo" with your GitHub repository URL
+  const githubRepoUrl = "https://github.com/yourusername/yourrepo";
+  
+  // Open the GitHub repo link in a new tab
+  window.open(githubRepoUrl, "_blank");
 };
 </script>
 
@@ -13,7 +29,7 @@ const handleClick = () => {
     <v-card>
         <v-navigation-drawer class="bg-primary" permanent>
             <v-list color="transparent">
-                <div class="add-contact">
+                <div class="add-contact my-4">
                     <AddContact />
                 </div>
                 <v-list-item class="ml-4">
@@ -36,11 +52,22 @@ const handleClick = () => {
 
             <template v-slot:append>
                 <div class="pa-2">
-                    <v-btn block>
-                        Logout
-                    </v-btn>
+                    <v-row align="center" justify="center">
+                        <v-col cols="auto">
+                            <v-btn density="compact" icon="mdi-cloud-upload"></v-btn>
+                        </v-col>
+
+                        <v-col cols="auto">
+                            <v-btn density="compact" icon="mdi-github"></v-btn>
+                        </v-col>
+
+                        <v-col cols="auto">
+                            <v-btn density="compact" icon="mdi-linkedin"></v-btn>
+                        </v-col>
+                    </v-row>
                 </div>
             </template>
+
         </v-navigation-drawer>
     </v-card>
 </template>
@@ -74,6 +101,7 @@ const handleClick = () => {
     justify-content: center;
     align-items: center;
     height: 100%;
-    /* adjust as needed */
 }
+
+
 </style>
